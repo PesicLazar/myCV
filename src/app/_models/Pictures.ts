e92b25c3c194d8097a39f8f3159b485166a9f93c
PesicLazar <lp2017230143@gmail.com>
2025-04-03 18:38:51 +0300

    fixed pics src

--- src/app/_models/Pictures.ts
+++ src/app/_models/Pictures.ts
@@ -1,9 +1,9 @@
 export class Pic{
-    static readonly picGraphic = new Pic ('Graphics', '/myCV/assets/project_snippets/computer_grafics.png');
-    static readonly picMining = new Pic ('Mining', '/myCV/assets/project_snippets/mining.png');
-    static readonly picAi = new Pic ('AI', '/myCV/assets/project_snippets/Ai_finnal.png');
+    static readonly picGraphic = new Pic ('Graphics', '/assets/project_snippets/computer_grafics.png');
+    static readonly picMining = new Pic ('Mining', '/assets/project_snippets/mining.png');
+    static readonly picAi = new Pic ('AI', '/assets/project_snippets/Ai_finnal.png');
     static readonly picPig = new Pic('Pig Game', '');// Placeholder for a future image
-    static readonly picVision = new Pic ('Computer Vision', '/myCV/assets/project_snippets/computer_vision.png');
+    static readonly picVision = new Pic ('Computer Vision', '/assets/project_snippets/computer_vision.png');
 
 
     private constructor(
