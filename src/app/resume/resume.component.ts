f48c698f9facd6a4be9bb9e586f3b7a45fd9c06d
PesicLazar <lp2017230143@gmail.com>
2025-04-03 18:55:11 +0300

    rec letter and updated resume with new links to verca

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -9,9 +9,16 @@ export class ResumeComponent {
 
   downloadPDFWeb() {
     const link = document.createElement('a');
-    link.href = 'assets/IT CV.pdf'; 
+    link.href = 'assets/IT CV.pdf';  //updated* 4.3.2025
     link.download = 'Resume.pdf';
     link.click();
   }
+  downloadDocx() {
+    const link = document.createElement('a');
+    link.href = 'assets/Рекоменд письмо.docx'; 
+    link.download = 'Рекоменд письмо.docx';
+    link.click();
+  }
+
 
 }
